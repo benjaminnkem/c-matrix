8bf86fdd86dcba8954a5c0beaf84731c1b55b775
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-02-03 19:45:45 +0100

    added basic user input

--- main.c
+++ main.c
@@ -3,4 +3,48 @@
 int main()
 {
   // Matrix solving problem
+  int a1;
+  int a2;
+  int a3;
+  int a4;
+
+  int b1;
+  int b2;
+  int b3;
+  int b4;
+
+  printf("enter a1: ");
+  scanf("%d", &a1);
+
+  printf("enter a2: ");
+  scanf("%d", &a2);
+
+  printf("enter a3: ");
+  scanf("%d", &a3);
+
+  printf("enter a4: ");
+  scanf("%d", &a4);
+
+  printf("enter b1: ");
+  scanf("%d", &b1);
+
+  printf("enter b2: ");
+  scanf("%d", &b2);
+
+  printf("enter b3: ");
+  scanf("%d", &b3);
+
+  printf("enter b4: ");
+  scanf("%d", &b4);
+
+  printf("%d", a1);
+  printf("%d", a2);
+  printf("%d", a3);
+  printf("%d", a4);
+  printf("%d", b1);
+  printf("%d", b2);
+  printf("%d", b3);
+  printf("%d", b4);
+
+  return 0;
 }
